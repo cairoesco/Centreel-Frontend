ae85a893394396289bb34b9f7d0e015384b9ec33
abdulkarimmeman <abdul@152tech.com>
2020-10-08 09:55:30 +0530

    Changes for filters

--- src/app/products/filter-dialog/filter-dialog.component.ts
+++ src/app/products/filter-dialog/filter-dialog.component.ts
@@ -4,6 +4,7 @@ import * as _ from 'lodash';
 import { Router } from '@angular/router';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { ApiService } from '../../api.service';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-filter-dialog',
@@ -18,10 +19,24 @@ export class FilterDialogComponent implements OnInit {
   productSubCategory = [];
   alltags: string[] = [];
   form_obj: any = new Object();
-
+  minDate = moment("2018-01-01");
+  maxDate = moment();
+  localconfi: any = { applyLabel: 'ok', separator: ' To ', format: 'DD/MM/YYYY', direction: 'ltr', weekLabel: 'W', cancelLabel: 'Cancel', customRangeLabel: 'Custom range', daysOfWeek: moment.weekdaysMin(), monthNames: moment.monthsShort(), firstDay: moment.localeData().firstDayOfWeek() }; 
   product_type_id: any;
   product_category_id: any;
 
+   //datepicker range
+   selected: any;
+   alwaysShowCalendars: boolean;
+   ranges: any = {
+     'Today': [moment(), moment()],
+     'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+     'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+     'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+     'This Month': [moment().startOf('month'), moment().endOf('month')],
+     'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+   }
+   //datepicker range
   constructor(private api: ApiService, public dialogRef: MatDialogRef<FilterDialogComponent>, @Inject(MAT_DIALOG_DATA) public data: any, public router: Router, public fb: FormBuilder) {
 
   }
@@ -38,6 +53,8 @@ export class FilterDialogComponent implements OnInit {
       product_type_id: [[]],
       product_category_id: [[]],
       tags: [[]],
+      selected: [''],
+      barcode_type: [''],
     });
   }
   applyFilter() {
