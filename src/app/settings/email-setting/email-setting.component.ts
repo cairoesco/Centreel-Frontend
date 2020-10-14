a7fc2c87ecc0c04b7ef3c23aaf77161d7be503f9
abdulkarimmeman <abdul@152tech.com>
2020-10-14 11:56:58 +0530

    Added changes for test mail create

---
+++ src/app/settings/email-setting/email-setting.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-email-setting',
+  templateUrl: './email-setting.component.html',
+  styleUrls: ['./email-setting.component.scss']
+})
+export class EmailSettingComponent implements OnInit {
+
+  public form: FormGroup;
+  filter_name: any;
+  portList: any = [];
+
+
+  constructor(private fb: FormBuilder, public dialogRef: MatDialogRef<EmailSettingComponent>, @Inject(MAT_DIALOG_DATA) public data: FilterDialogData) {
+    this.form = this.fb.group({
+      host: [null, Validators.compose([Validators.required, Validators.minLength(5), Validators.maxLength(10)])],
+      port: [null, Validators.compose([Validators.required, Validators.minLength(5), Validators.maxLength(10)])],
+      subject: [null, Validators.compose([Validators.required])],
+      description: [null, Validators.compose([Validators.required])],
+    });
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close(this.form.value);
+  }
+  ngOnInit() {
+    // this.form = this.fb.group({
+    //   filter_name: [null, Validators.compose([Validators.required, Validators.minLength(5), Validators.maxLength(10)])],
+    // });
+  }
+
+  /* close dialog box */
+  close() {
+    this.dialogRef.close();
+  }
+  /* close dialog box */
+
+}
+export interface FilterDialogData {
+  animal: string;
+  name: string;
+}
--- src/app/settings/smtp-setting/smtp-setting.component.ts
+++ src/app/settings/smtp-setting/smtp-setting.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { EmailSettingComponent } from '../email-setting/email-setting.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-smtp-setting',
@@ -7,32 +9,54 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
   styleUrls: ['./smtp-setting.component.scss']
 })
 export class SmtpSettingComponent implements OnInit {
-  portList:any=['25','465','587'];
-  settingForm:FormGroup;
+  portList: any = ['25', '465', '587'];
+  settingForm: FormGroup;
 
-  constructor(public fb:FormBuilder) {    
-   }
+  constructor(public fb: FormBuilder, public dialog: MatDialog) {
+  }
 
   ngOnInit() {
     this.InitializeForm();
   }
+  openEmailSetting() {
+    let formData = this.settingForm.value;
+    const dialogRef = this.dialog.open(EmailSettingComponent, {
+      width: '70%',
+      maxWidth: "700px",
+      data: { formData }
+    });
+    //Call after delete confirm model close
+    dialogRef.afterClosed().subscribe(result => {
+      if (Boolean(result)) {
+        //   this.inProgress = true;
+        // this.isLoading = true;
+        //   this.productobj['pageSize'] = 20;
+        //   this.productobj['pageIndex'] = 0;
+        //   result.product_type_id ? this.productobj['product_type_id'] = result.product_type_id ? JSON.stringify(result.product_type_id) : '' : delete this.productobj['product_type_id'];
+        //   result.product_category_id ? this.productobj['product_category_id'] = result.product_category_id ? JSON.stringify(result.product_category_id) : '' : delete this.productobj['product_category_id'];
+        //   result.tags ? this.productobj['tags'] = result.tags ? JSON.stringify(result.tags) : '' : delete this.productobj['tags'];
 
+        //   this.filter_data = result;
+        //   this.getVariantData();
+      }
+    });
+  }
   //Initialize setting form
-  InitializeForm(){
-    this.settingForm=this.fb.group({
-      host:['',Validators.compose([Validators.required])],
-      port:['',Validators.compose([Validators.required])],
-      username:['',Validators.compose([Validators.required])],
-      password:['',Validators.compose([Validators.required])],
-      connection_type:['']
+  InitializeForm() {
+    this.settingForm = this.fb.group({
+      host: ['', Validators.compose([Validators.required])],
+      port: ['', Validators.compose([Validators.required])],
+      username: ['', Validators.compose([Validators.required])],
+      password: ['', Validators.compose([Validators.required])],
+      connection_type: ['']
     })
   }
 
   //Save settings data
-  SaveSettings(data){
-    if(Boolean(this.settingForm.valid)){
+  SaveSettings(data) {
+    if (Boolean(this.settingForm.valid)) {
       console.log(data);
     }
-    
+
   }
 }
