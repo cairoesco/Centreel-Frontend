2f38f35eebba98db74ad3b34365f4236bf3f4fbf
Shreyansh Hingar <shreyansh@152tech.com>
2021-02-04 12:21:28 +0530

    CEN-298
    CEN-298 Support added for the daterange feature.

--- src/app/reports/cogs-report/cogs-report.component.ts
+++ src/app/reports/cogs-report/cogs-report.component.ts
@@ -75,6 +75,13 @@ export class CogsReportComponent implements OnInit {
         edate = moment(val.selected.end, 'DD/MM/YYYY HH:mm:ss').add(1, 'day').format('YYYY-MM-DD HH:mm:ss');
         this.formobj.enddate = sdate;
       }
+      this.formobj.from_date = this.utils.get_utc_from_to_date(sdate);
+      //this.formobj.end_date = this.utils.get_utc_from_to_date(edate);
+      this.formobj.to_date = this.utils.get_utc_from_to_date(edate);
+
+      var display_date = moment(val.selected.start, 'DD/MM/YYYY HH:mm:ss').format('DD/MM/YYYY');
+      this.export_date = moment(val.selected.start, 'DD/MM/YYYY HH:mm:ss').format('MMMDDYYYY');
+
       var start_date = moment(sdate).format('YYYY-MM-DD HH:mm:ss');
       var to_date = moment(edate).format('YYYY-MM-DD HH:mm:ss');
       this.formobj.from_date = this.utility.get_utc_from_to_date(start_date);
