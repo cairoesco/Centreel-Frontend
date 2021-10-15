acbe29d8215e768e5e1ced65f4d16a98aefb30fe
zuby-152 <nzube@152tech.com>
2021-10-15 15:08:49 +0100

    add customer feature

--- src/app/customer/customer.service.ts
+++ src/app/customer/customer.service.ts
@@ -10,10 +10,15 @@ export class CustomerService {
     GetCustomerList(params) {
         return this.webApi.get('patients', params)
     }
-    getRawDetail(){
-        return this.webApi.get('patients/create')        
+    AddCustomer(params) {
+        return this.webApi.post('patients', params)
     }
-
+    getRawDetail(){
+        return this.webApi.get('users/create')        
+    }
+    getLocationList(type, parent_id) {
+        return this.webApi.get('location?location_type=' + type + '&parent_id=' + parent_id);
+    }
     getCustomerPreferredProductList(id) {
         return this.webApi.get('patients/'+id+'/product')
     }
