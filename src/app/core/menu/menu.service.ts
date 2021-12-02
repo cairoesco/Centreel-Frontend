b61dff99effc776bd078e9d4569d1a1ead62c36a
zuby-152 <76101721+zuby-152@users.noreply.github.com>
2021-12-02 16:24:34 +0100

    franchise user role (#107)

--- src/app/core/menu/menu.service.ts
+++ src/app/core/menu/menu.service.ts
@@ -205,7 +205,7 @@ const MENUITEMS_FRANCHISE = [
     method: 'sales',
     children: [
       { state: 'closeout', name: 'Closeout', module: 'reports', method: 'sales' },
-      { state: 'customsales', name: 'Custom sales', module: 'reports', method: 'sales' },
+      // { state: 'customsales', name: 'Custom sales', module: 'reports', method: 'sales' },
       // { state: 'dailyinterim', name: 'Daily interim', module: 'reports', method: 'sales' },
       // { state: 'employee-sales', name: 'Employee Sales', module: 'reports', method: 'sales' },
       // { state: 'inventory', name: 'Inventory Report', module: 'reports', method: 'sales' },
