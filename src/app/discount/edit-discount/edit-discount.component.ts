d0668532a0bc1568dc6f4c58b1cb266519a2c974
zuby-152 <nzube@152tech.com>
2021-12-29 13:43:08 +0100

    implemented discount pending feedback

--- src/app/discount/edit-discount/edit-discount.component.ts
+++ src/app/discount/edit-discount/edit-discount.component.ts
@@ -53,9 +53,16 @@ export class EditDiscountComponent implements OnInit {
 	public SelectedParameters: any = [];
 	public tempEntityArray: any = [];
 	public selectedTag: any = [];
+	public discountModeArray: any = [];
 	public discountForm: FormGroup;
 	public index: any = "ALL";
 	public rawDetail: any;
+
+	public allTagsArray: any = [];
+	public allCategoryArray: any = [];
+
+	public discountType: any;
+
 	separatorKeysCodes: number[] = [ENTER, COMMA];
 	tagEntities: string[] = [];
 	allTagEntities: any = [];
@@ -122,6 +129,7 @@ export class EditDiscountComponent implements OnInit {
 	getDiscountForm() {
 		this.discountForm = this.fb.group({
 			discount_name: "",
+			discount_mode: [],
 			discount_type: [],
 			status: [],
 			parameters: [],
@@ -149,8 +157,34 @@ export class EditDiscountComponent implements OnInit {
 
 	handleOnSubmitGeneralEdit() {
 		this.inProgress = true;
-		this.discountService.updateDiscount(this.discount_id, this.discountForm.value).subscribe((payload: any) => {
-			if (payload.success) {
+		let tempParameterData = [];
+		let tempParameterPayload;
+		if (this.discountType == "category") {
+			for (const params in this.discountForm.value.parameters) {
+				const filtered = this.allCategoryArray.find((item) => item.product_category_id === this.discountForm.value.parameters[params]);
+				tempParameterData.push(filtered);
+			}
+			tempParameterPayload = JSON.stringify(tempParameterData);
+		} else if (this.discountType == "tag") {
+			for (const params in this.discountForm.value.parameters) {
+				const filtered = this.allTagsArray.find((item) => item.id === this.discountForm.value.parameters[params]);
+				tempParameterData.push(filtered);
+			}
+			tempParameterPayload = JSON.stringify(tempParameterData);
+		}
+		const payload = {
+			title: this.discountForm.value.discount_name,
+			discount_mode: this.discountForm.value.discount_mode,
+			type: this.discountForm.value.discount_type,
+			value: this.discountForm.value.discount_value,
+			min_qty: this.discountForm.value.min_qty,
+			product_weight: this.discountForm.value.product_weight,
+			status: this.discountForm.value.status,
+			paramters: tempParameterPayload,
+		};
+
+		this.discountService.updateDiscount(this.discount_id, payload).subscribe((response: any) => {
+			if (response.success) {
 			}
 		});
 		this.inProgress = false;
@@ -158,32 +192,39 @@ export class EditDiscountComponent implements OnInit {
 
 	GetDiscount(id) {
 		this.inProgress = true;
-		this.discountService.GetStores().subscribe((response: any) => {
-			if (response.success) {
-				this.discountService.GetDiscountList(response.data[0].store_id).subscribe((payload: any) => {
-					if (payload.success) {
-						const tempDiscountData = payload.data.filter((discount) => discount.discount_id == id);
-						console.log(tempDiscountData[0]);
-						if (tempDiscountData) {
-							this.discountForm.get("discount_name").setValue(tempDiscountData[0].title);
-							this.discountForm.get("status").setValue(tempDiscountData[0].status);
-							this.discountForm.get("discount_type").setValue(tempDiscountData[0].type);
-							this.discountForm.get("parameters").setValue(tempDiscountData[0].paramters);
-							this.discountForm.get("discount_value").setValue(tempDiscountData[0].value);
-							this.discountForm.get("min_qty").setValue(tempDiscountData[0].min_qty);
-							this.discountForm.get("product_weight").setValue(tempDiscountData[0].product_weight);
+		this.discountService.GetSingleDiscount(id).subscribe((payload: any) => {
+			if (payload.success) {
+				if (payload.data) {
+					this.discountForm.get("discount_name").setValue(payload.data.title);
+					this.discountForm.get("status").setValue(payload.data.status);
+					this.discountForm.get("discount_type").setValue(payload.data.type);
+					this.discountForm.get("discount_mode").setValue(payload.data.discount_mode);
+					this.discountType = payload.data.type;
 
-							this.DiscounParametersArray = tempDiscountData[0].paramters ?? [];
+					const tempParams = [];
+					const tempPayload = JSON.parse(payload.data.paramters);
 
-							this.SelectedParameters.push(this.DiscounParametersArray);
-							this.discount_id = id;
+					for (const parameter in tempPayload) {
+						if (payload.data.type == "category") {
+							tempParams.push(Number(tempPayload[parameter].cat_id));
+						} else if (payload.data.type == "tag") {
+							tempParams.push(Number(tempPayload[parameter].tag_id));
 						}
 					}
-				});
+					this.discountForm.get("parameters").setValue([...tempParams]);
+					this.discountForm.get("discount_value").setValue(payload.data.value);
+					this.discountForm.get("min_qty").setValue(payload.data.min_qty);
+					this.discountForm.get("product_weight").setValue(payload.data.product_weight);
+
+					this.DiscounParametersArray = tempPayload ?? [];
+
+					this.SelectedParameters.push(this.DiscounParametersArray);
+					this.discount_id = id;
+				}
 			}
-			this.inProgress = false;
-			this.viewOnly();
 		});
+		this.inProgress = false;
+		this.viewOnly();
 	}
 
 	ngOnInit() {
@@ -197,6 +238,25 @@ export class EditDiscountComponent implements OnInit {
 				status_id: 1,
 			},
 		];
+		this.discountModeArray = [
+			{
+				value: "$",
+				item: "Amount ($)",
+			},
+			{
+				value: "%",
+				item: "Percentage (%)",
+			},
+		];
+
+		this.discountService.GetAddDiscountData().subscribe((payload: any) => {
+			for (const item of payload.data.categories) {
+				this.allCategoryArray.push(item);
+			}
+			for (const item of payload.data.tags) {
+				this.allTagsArray.push(item);
+			}
+		});
 
 		this.activatedRoute.params.subscribe((params: Params) => {
 			if (params?.id) {
@@ -205,6 +265,7 @@ export class EditDiscountComponent implements OnInit {
 		});
 		this.getDiscountForm();
 	}
+
 	ngDoCheck() {
 		this.innerHeight = window.innerHeight - 192;
 	}
