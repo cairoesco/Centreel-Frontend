64b36ca8b946fa186d3f3cc7e02c3f485b921bf7
cairovirk <68035553+cairovirk@users.noreply.github.com>
2024-06-21 12:06:17 -0700

    Revert "Revert "change base url""

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -21,7 +21,7 @@
 export const environment = {
   production: false,
   // baseUrl: 'http://devapi.centreel.ca/',
-  baseUrl: 'https://devapi.centreel.app/',
+  baseUrl: 'https://centreel-elb-1171899288.ca-central-1.elb.amazonaws.com/',
   client_id: 2,
   client_secret: "86uYm59gFcLwrnUcHZvQ3eXHwiCJiZDVaqA5t863",
   GOOGLE_API_BROWSER_KEY: "AIzaSyCGUPzlHtZaH_RWIWzsSvKE7gh1oU3rCOs",
