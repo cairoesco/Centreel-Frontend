5fae7a38993e43f4ce90baf92e267cff8b36ae91
Aman Ullah <aman.ullah_contractor@gaintheory.com>
2024-06-27 16:27:21 +0500

    backend url changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://api.centreel.test.com/',
+  baseUrl: 'https://api.centreel.app/',
   client_id: 2,
   client_secret: "86uYm59gFcLwrnUcHZvQ3eXHwiCJiZDVaqA5t863",
   GOOGLE_API_BROWSER_KEY: "AIzaSyCGUPzlHtZaH_RWIWzsSvKE7gh1oU3rCOs",
